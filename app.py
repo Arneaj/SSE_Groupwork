0e4eb8fe1ff9467d72ca3861b64f992de5bb28b7
Arneaj <arnaud.rollandmail@gmail.com>
2024-12-01 12:41:33 +0000

    corrected import of requests

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify, requests
+from flask import Flask, render_template, request, jsonify
+import requests
 from sqlalchemy import create_engine, text
 #from dotenv import load_dotenv
 import os
